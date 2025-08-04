7a42f6c91453bc84643f3479ab76e7d2b5d273a3
dhanush80888 <dhanuds586@gmail.com>
2025-08-04 20:23:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const BASE_URL = "https://raw.githubusercontent.com/WoXy-Sensei/currency-api/main/api/"
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
-const msg=document.querySelector("form .msg");
+const msg = document.querySelector("form .msg");
+
+
 
 let dropdowns = document.querySelectorAll(".dropdown select");
 let btn = document.querySelector("form button");
@@ -25,6 +27,7 @@ for (let select of dropdowns) {
     })
 }
 
+
 const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
@@ -41,16 +44,45 @@ btn.addEventListener("click", async (evt) => {
         amtVal = 1;
         amount.value = "1";
     }
-    const URL = `${BASE_URL}${toCurr.value}_${fromCurr.value}.json`;
+    const URL = `${BASE_URL}${fromCurr.value}_${toCurr.value}.json`;
     let response = await fetch(URL);
     let data = await response.json();
     let Rate = data.rate;
-    let Final_amount=amtVal*Rate;
+    let Final_amount = amtVal * Rate;
     // console.log(Final_amount)
-    msg.innerHTML=`${amtVal} ${fromCurr.value} = ${Final_amount} ${toCurr.value}`
+    msg.innerHTML = `${amtVal} ${fromCurr.value} = ${Final_amount} ${toCurr.value}`
 
 
 
 })
 
+
+let arrow =  document.querySelector(".arrow")
+arrow.addEventListener("click", () => {
+    if((arrow.src.includes("arrow1.svg"))){
+        arrow.src="arrow2.svg"
+    }
+    else{
+        arrow.src="arrow1.svg"
+    }
+    
+    temp_fromcurr=fromCurr.value
+    let from_options=fromCurr.getElementsByTagName("option")
+    // console.log(from_options)
+    for(let i in from_options){
+        if(from_options[i].value==toCurr.value){
+            from_options[i].selected="selected"
+            updateFlag(fromCurr)
+        }
+    }
+
+    let to_options=toCurr.getElementsByTagName("option")
+    // console.log(to_options)
+    for(let i in to_options){
+        if(to_options[i].value==temp_fromcurr){
+            to_options[i].selected="selected"
+            updateFlag(toCurr)
+        }
+    }
+})
 
